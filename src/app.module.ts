ec2e67821c2c9ff8353476f8867e81398e30d419
arteagaluis <arteagaluis123@gmail.com>
2026-06-06 16:35:25 -0500

    feat: Config Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: envValidationSchema,
     }),
   ],
   controllers: [AppController],
